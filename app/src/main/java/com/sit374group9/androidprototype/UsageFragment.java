a5476d760331af4ceb278d74c95184d7bad24211
Rob Cunning <rjacunning@gmail.com>
2017-12-17 13:10:32 +1100

    Refactored stuff, added usage field

--- app/src/main/java/com/sit374group9/androidprototype/UsageFragment.java
+++ app/src/main/java/com/sit374group9/androidprototype/UsageFragment.java
@@ -1,10 +1,25 @@
 package com.sit374group9.androidprototype;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by robcunning on 16/12/17.
@@ -12,10 +27,98 @@ import android.view.ViewGroup;
 
 public class UsageFragment extends Fragment {
 
-    private static final String TAG = "UserFragment";
+    private static final String TAG = "UsageFragment";
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
+    private String userID;
+    private String userUsage;
+
+    private TextView textUsage;
 
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle savedInstanceState) {
         return layoutInflater.inflate((R.layout.fragment_usage), container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstaceState) {
+        super.onActivityCreated(savedInstaceState);
+
+        textUsage = (TextView) getActivity().findViewById(R.id.account_usage_value);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        assert user != null;
+        userID = user.getUid();
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "user is signed in " + user.getUid() + " " + user.getEmail());
+                } else {
+                    Log.d(TAG, "user is logged out");
+                }
+            }
+        };
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "" + dataSnapshot.getValue());
+                showData(dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void showData(Object object) {
+        Log.d(TAG, "" + object);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(object);
+
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONObject userObject = jsonObject.getJSONObject("user");;
+            JSONObject usersObject = userObject.getJSONObject("users");
+            JSONObject userDetailsObject = usersObject.getJSONObject(userID);
+
+            Log.d(TAG, userDetailsObject.toString());
+
+            userUsage = userDetailsObject.getString("usage");
+
+            textUsage.setText(userUsage);
+
+            Log.d(TAG, userUsage);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            firebaseAuth.removeAuthStateListener(authListener);
+        }
     }
 }
