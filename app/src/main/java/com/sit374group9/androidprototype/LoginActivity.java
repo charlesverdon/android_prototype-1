6e96b2925a87aa7dd11e5ffd7eee031900881544
Rob Cunning <rjacunning@gmail.com>
2018-04-17 09:07:34 +1000

    removed unwanted files

--- app/src/main/java/com/sit374group9/androidprototype/LoginActivity.java
+++ app/src/main/java/com/sit374group9/androidprototype/LoginActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -28,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
     EditText editUsername;
     EditText editPassword;
 
+
     TextView textErrorLogin;
 
     Button loginButton;
@@ -45,11 +50,27 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        TextView tv2=(TextView) this.findViewById(R.id.textView2);
+        String text="Forget your Password? Click Here";
+        SpannableString ss=new SpannableString(text);
+        ss.setSpan(new ClickableSpan() {
+
+            @Override
+            public void onClick(View widget) {
+                Intent i=new Intent(LoginActivity.this,ForgetPassword.class);
+                startActivity(i);
+
+            }
+        }, 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv2.setText(ss);
+        tv2.setMovementMethod(LinkMovementMethod.getInstance());
+
         setup();
     }
 
     private void setup() {
         firebaseAuth = FirebaseAuth.getInstance();
+
 
         editUsername = (EditText) findViewById(R.id.edit_username);
         editPassword = (EditText) findViewById(R.id.edit_password);
