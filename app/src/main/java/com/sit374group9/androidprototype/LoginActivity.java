a47b3c9d166633df77f1d40c041a2e36801453c9
Rob Cunning <rjacunning@gmail.com>
2018-05-14 10:13:49 +1000

    Added terms and conditions button to main activity

--- app/src/main/java/com/sit374group9/androidprototype/LoginActivity.java
+++ app/src/main/java/com/sit374group9/androidprototype/LoginActivity.java
@@ -69,12 +69,12 @@ public class LoginActivity extends AppCompatActivity {
 
         TextView tv2 = (TextView) this.findViewById(R.id.textView2);
         String text = (String) "Forgot Password";
-        SpannableString ss=new SpannableString(text);
+        SpannableString ss = new SpannableString(text);
         ss.setSpan(new ClickableSpan() {
 
             @Override
             public void onClick(View widget) {
-                Intent i=new Intent(LoginActivity.this,ForgotPasswordActivity.class);
+                Intent i = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
                 startActivity(i);
 
             }
@@ -101,7 +101,7 @@ public class LoginActivity extends AppCompatActivity {
             loading.setVisibility(View.GONE);
             loginButton.setVisibility(View.VISIBLE);
         } else {
-            firebaseAuth.signInWithEmailAndPassword(username, password) .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            firebaseAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     loading.setVisibility(View.GONE);
--- app/src/main/java/com/sit374group9/androidprototype/MainActivity.java
+++ app/src/main/java/com/sit374group9/androidprototype/MainActivity.java
@@ -4,7 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +27,21 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
         actionBar.hide();
+
+        TextView tv2 = (TextView) this.findViewById(R.id.textView2);
+        String text = (String) "Terms and Conditions";
+        SpannableString ss = new SpannableString(text);
+        ss.setSpan(new ClickableSpan() {
+
+            @Override
+            public void onClick(View widget) {
+                Intent i = new Intent(MainActivity.this, PlaceholderActivity.class);
+                startActivity(i);
+
+            }
+        }, 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv2.setText(ss);
+        tv2.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public void goToLogin(View view) {
